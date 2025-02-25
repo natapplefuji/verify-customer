cc1a6e0c0216de9b58e50764cc7a2142608bab9c
natthida.j <natthida@orcsoft.co.th>
2025-02-25 22:46:13 +0700

    fix refresh

--- src/app/page/info/components/info/info.component.ts
+++ src/app/page/info/components/info/info.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.scss']
 })
-export class InfoComponent implements OnInit {
+export class InfoComponent implements OnInit,OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
     console.log('info')
+    window.onbeforeunload = () => {
+      sessionStorage.removeItem('auth_token');
+      sessionStorage.removeItem('userProfile');
+    };
+  }
+  ngOnDestroy(): void {
+    window.onbeforeunload = null;
   }
 
 }
